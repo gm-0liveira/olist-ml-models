10fe8b7137b85b9c11259e740ca58445c98b7d9f
gm_0liveira <gmarcieoliveira@gmail.com>
2023-11-23 18:00:41 -0300

    Variáveis de Pedidos por Estado

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -6,7 +6,7 @@ WITH tb_pedidos AS (
   FROM pedido AS t1
     LEFT JOIN item_pedido AS t2 ON t1.idPedido = t2.idPedido
   WHERE t1.dtPedido < '2018-01-01'
-    AND t1.dtPedido >= julianday('2018-01-01', '-6 month')
+    AND t1.dtPedido >= julianday('2018-01-01', '-6 months')
     AND idVendedor IS NOT NULL
 ),
 tb_join AS (
